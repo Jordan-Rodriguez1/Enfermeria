6d4ffd902a8379be1f7cac70348dc2897ceef5c5
Jordan (Laptop) <miguel20010807@gmail.com>
2023-05-13 20:34:40 -0600

    Reinicio Base de Datos

--- Assets/enfermeria.sql
+++ Assets/enfermeria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2023 a las 04:25:55
+-- Tiempo de generación: 14-05-2023 a las 04:33:32
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -47,9 +47,10 @@ CREATE TABLE `alumnos` (
 --
 
 INSERT INTO `alumnos` (`id`, `usuario`, `nombre`, `correo`, `grado`, `grupo`, `asistencias`, `faltas`, `rol`, `clave`, `estado`, `perfil`) VALUES
-(1, '666', 'Junita Pérez', 'juntio@ucol.mx', 9, 'B', 0, 1, 1, 'c7e616822f366fb1b5e0756af498cc11d2c0862edcb32ca65882f622ff39de1b', 1, 'perfil.jpg'),
-(2, '20166608', 'Miguel Jordán Rodríguez Reyes', 'mrodriguez@ucol.mx', 6, 'C', 1, 0, 1, '537172e1d014c070692080f30925bd8757175f77b4fb0e1b377d1f93a7b85e16', 1, 'perfil.jpg'),
-(3, '123', 'Prueba2', 'prueba2@ucol.mx', 9, 'A', 6, 2, 1, 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, 'perfil.jpg');
+(1, '20230511', 'Mark Elliot Zuckerberg', 'alum1@ucol.mx', 1, 'A', 0, 0, 1, '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(2, '20230512', 'Timothy Donald Cook', 'alum2@ucol.mx', 1, 'C', 0, 0, 1, '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(3, '20230513', 'Steven Paul Jobs', 'alum3@ucol.mx', 2, 'B', 0, 0, 1, '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(4, '20230514', 'Jeffrey Preston Bezos', 'alum4@ucol.mx', 2, 'A', 0, 0, 1, '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,14 +65,6 @@ CREATE TABLE `asistencias` (
   `asistencia` int(5) NOT NULL DEFAULT 1,
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
-
---
--- Volcado de datos para la tabla `asistencias`
---
-
-INSERT INTO `asistencias` (`id`, `id_practica`, `id_alumno`, `asistencia`, `fecha`) VALUES
-(1, 1, 1, 0, '2023-04-26 00:00:00'),
-(2, 1, 2, 2, '2023-05-07 19:29:40');
 
 -- --------------------------------------------------------
 
@@ -89,10 +82,7 @@ CREATE TABLE `categorias` (
 --
 
 INSERT INTO `categorias` (`id`, `nombre`) VALUES
-(1, 'Genérica'),
-(23, 'Jeringa'),
-(24, 'Medicamento'),
-(26, 'XXX');
+(1, 'Genérica');
 
 -- --------------------------------------------------------
 
@@ -109,18 +99,6 @@ CREATE TABLE `compras` (
   `fecha` datetime NOT NULL DEFAULT current_timestamp(),
   `formato` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `compras`
---
-
-INSERT INTO `compras` (`id`, `descripcion`, `total`, `id_proveedor`, `id_generador`, `fecha`, `formato`) VALUES
-(37, 'Primera Prueba', 1400.00, '6', '14', '2023-04-16 13:46:19', 'LOGO.jpg'),
-(38, 'Prueba 2', 200.00, '1', '14', '2023-04-16 13:47:30', '38.pdf'),
-(39, 'Holaaa', 700.00, '6', '14', '2023-04-16 14:16:15', ''),
-(40, 'Quedó?', 100.00, '6', '14', '2023-04-16 23:43:29', ''),
-(41, 'Prueba Gestor', 100.00, '6', '26', '2023-04-17 11:21:00', ''),
-(42, 'Prueba X', 100.00, '1', '14', '2023-04-22 15:17:04', '');
 
 -- --------------------------------------------------------
 
@@ -145,9 +123,7 @@ INSERT INTO `configsemestre` (`id`, `aminimas`, `fmaximas`) VALUES
 (4, 3, 3),
 (5, 3, 3),
 (6, 3, 3),
-(7, 3, 3),
-(8, 3, 3),
-(9, 3, 3);
+(7, 3, 3);
 
 -- --------------------------------------------------------
 
@@ -170,7 +146,7 @@ CREATE TABLE `configuracion` (
 --
 
 INSERT INTO `configuracion` (`id`, `facultad`, `calle`, `colonia`, `cp`, `ciudad`, `semestres`) VALUES
-(1, 'Facultad de Enfermería', 'Av. Universidad No. 333', 'Las Víboras', 28040, 'Colima, Colima, México.', 9);
+(1, 'Facultad de Enfermería', 'Av. Universidad No. 333', 'Las Víboras', 28040, 'Colima, Colima, México.', 7);
 
 -- --------------------------------------------------------
 
@@ -185,13 +161,6 @@ CREATE TABLE `cotizaciones` (
   `estado` tinyint(4) NOT NULL DEFAULT 1,
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `cotizaciones`
---
-
-INSERT INTO `cotizaciones` (`id`, `descripcion`, `total`, `estado`, `fecha`) VALUES
-(47, 'Prueba 1', 2, 1, '2023-04-22 15:26:32');
 
 -- --------------------------------------------------------
 
@@ -210,23 +179,6 @@ CREATE TABLE `detalle_compra` (
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `detalle_compra`
---
-
-INSERT INTO `detalle_compra` (`id`, `id_compra`, `producto`, `id_producto`, `cantidad`, `precio`, `id_usuario`, `fecha`) VALUES
-(29, 37, 'Prueba', 254, 3.00, 100.00, 14, '2023-04-16 13:46:19'),
-(30, 37, 'Otro', 256, 2.00, 500.00, 14, '2023-04-16 13:46:19'),
-(31, 37, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-16 13:46:19'),
-(32, 38, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-16 13:47:30'),
-(33, 38, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-16 13:47:30'),
-(34, 39, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-16 14:16:15'),
-(35, 39, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-16 14:16:15'),
-(36, 39, 'Otro', 256, 1.00, 500.00, 14, '2023-04-16 14:16:15'),
-(37, 40, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-16 23:43:29'),
-(38, 41, 'Prueba', 254, 1.00, 100.00, 26, '2023-04-17 11:21:00'),
-(39, 42, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-22 15:17:04');
-
 -- --------------------------------------------------------
 
 --
@@ -241,14 +193,6 @@ CREATE TABLE `detalle_coti` (
   `id_usuario` int(11) NOT NULL,
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `detalle_coti`
---
-
-INSERT INTO `detalle_coti` (`id`, `id_cotizacion`, `id_producto`, `cantidad`, `id_usuario`, `fecha`) VALUES
-(53, 47, 253, 1.00, 14, '2023-04-22 15:26:32'),
-(54, 47, 254, 1.00, 14, '2023-04-22 15:26:32');
 
 -- --------------------------------------------------------
 
@@ -284,43 +228,6 @@ CREATE TABLE `detalle_venta` (
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `detalle_venta`
---
-
-INSERT INTO `detalle_venta` (`id`, `id_venta`, `producto`, `id_producto`, `cantidad`, `precio`, `id_usuario`, `fecha`) VALUES
-(49, 44, 'Ejemplo', 253, 2.00, 100.00, 1, '2023-02-22 23:58:59'),
-(50, 45, 'Ejemplo', 253, 2.00, 100.00, 1, '2023-02-22 23:59:11'),
-(51, 46, 'Ejemplo', 253, 3.00, 100.00, 1, '2023-02-23 00:02:26'),
-(52, 47, 'Ejemplo', 253, 1.00, 100.00, 1, '2023-03-24 10:11:57'),
-(53, 48, 'Ejemplo', 253, 1.00, 100.00, 1, '2023-04-06 12:41:14'),
-(54, 49, 'Prueba', 254, 1.00, 100.00, 1, '2023-04-06 12:46:29'),
-(55, 50, 'Prueba', 254, 1.00, 100.00, 1, '2023-04-06 13:13:13'),
-(56, 51, 'Ejemplo', 253, 1.00, 100.00, 1, '2023-04-06 13:17:26'),
-(57, 52, 'Prueba', 254, 1.00, 100.00, 1, '2023-04-06 13:18:17'),
-(58, 53, 'Ejemplo', 253, 1.00, 100.00, 1, '2023-04-06 13:19:39'),
-(59, 54, 'Ejemplo', 253, 1.00, 100.00, 1, '2023-04-06 13:21:50'),
-(60, 55, 'Prueba', 254, 1.00, 100.00, 1, '2023-04-06 13:22:27'),
-(61, 56, 'OTRO', 255, 1.00, 20.00, 1, '2023-04-06 13:23:23'),
-(62, 58, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-13 13:08:04'),
-(63, 58, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-13 13:08:04'),
-(64, 58, 'OTRO', 255, 1.00, 20.00, 14, '2023-04-13 13:08:04'),
-(65, 59, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-14 13:18:46'),
-(66, 61, 'Ejemplo', 253, 2.00, 100.00, 14, '2023-04-14 13:32:32'),
-(67, 61, 'Prueba', 254, 2.00, 100.00, 14, '2023-04-14 13:32:32'),
-(68, 61, 'OTRO', 255, 1.00, 20.00, 14, '2023-04-14 13:32:32'),
-(69, 62, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-14 13:38:10'),
-(70, 63, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-14 20:29:30'),
-(71, 64, 'Ejemplo', 253, 1.00, 100.00, 26, '2023-04-15 00:37:53'),
-(72, 65, 'Ejemplo', 253, 1.00, 100.00, 26, '2023-04-15 00:38:12'),
-(73, 65, 'Prueba', 254, 1.00, 100.00, 26, '2023-04-15 00:38:12'),
-(74, 65, 'OTRO', 255, 1.00, 20.00, 26, '2023-04-15 00:38:12'),
-(75, 66, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-15 01:31:14'),
-(76, 67, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-16 15:04:20'),
-(77, 67, 'Otro', 256, 1.00, 500.00, 14, '2023-04-16 15:04:20'),
-(78, 68, 'Ejemplo', 253, 1.00, 100.00, 14, '2023-04-26 10:31:25'),
-(79, 68, 'Prueba', 254, 1.00, 100.00, 14, '2023-04-26 10:31:25');
-
 -- --------------------------------------------------------
 
 --
@@ -337,16 +244,6 @@ CREATE TABLE `plantillas` (
   `estado` int(11) NOT NULL DEFAULT 1,
   `fecha` date NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
-
---
--- Volcado de datos para la tabla `plantillas`
---
-
-INSERT INTO `plantillas` (`id`, `nombre`, `descripcion`, `objetivo`, `requisitos`, `formato`, `estado`, `fecha`) VALUES
-(1, 'Prueba 1 Act', 'Hola Act', 'Adios Act', 'Hola Act', '', 1, '2023-04-21'),
-(2, 'Prueba 2 Act', 'Hola Act', 'Adios Act', 'Hola Act', '', 1, '2023-04-22'),
-(3, 'Prueba 3 Act', 'Adios', 'Hola', 'Adios', '', 1, '2023-04-22'),
-(4, 'Prueba 4', 'Hola', 'Adios', 'Hola', '', 1, '2023-04-22');
 
 -- --------------------------------------------------------
 
@@ -368,15 +265,6 @@ CREATE TABLE `practicas` (
   `fecha` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish2_ci;
 
---
--- Volcado de datos para la tabla `practicas`
---
-
-INSERT INTO `practicas` (`id`, `nombre`, `id_plantilla`, `id_plantillam`, `id_responsable`, `fecha_practica`, `semestre`, `capacidad`, `registros`, `estado`, `fecha`) VALUES
-(1, 'Prueba 1', 1, 47, 26, '2023-04-24 00:00:00', 6, 10, 2, 3, '2023-04-23 00:00:00'),
-(2, 'Prueba 2', 4, 47, 27, '2023-05-09 10:00:00', 6, 1, 0, 0, '2023-05-08 20:33:16'),
-(3, 'Prueba 3 (Aula 1)', 1, 47, 14, '2023-05-10 10:45:00', 5, 1, 0, 0, '2023-05-09 17:44:08');
-
 -- --------------------------------------------------------
 
 --
@@ -396,21 +284,6 @@ CREATE TABLE `productos` (
   `estado` tinyint(4) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
---
--- Volcado de datos para la tabla `productos`
---
-
-INSERT INTO `productos` (`id`, `codigo`, `nombre`, `cantidad`, `precio`, `minimo`, `categoria`, `proveedor`, `tipo`, `estado`) VALUES
-(253, '1', 'Ejemplo', 5.00, 100.00, 3, 'A', 'Guadalajara', 'Vigente', 1),
-(254, '2', 'Prueba', 11.00, 100.00, 6, 'Medicamento', 'Prueba', 'Vigente', 1),
-(256, '3', 'Otro', 20.00, 500.00, 0, 'Jeringa', 'Proveedor General', 'Caducado', 1),
-(260, '4', 'Prueba 4', 0.00, 200.00, 0, 'AAA', 'Proveedor General', 'Vigente', 1),
-(261, '5', 'Hola', 15.00, 200.00, 0, 'Jeringa', 'Prueba2', 'Caducado', 1),
-(262, '7', 'Otro Mendigo Ejemplo', 0.00, 300.00, 6, 'Genérica', 'Genérico', 'Vigente', 1),
-(276, '78', 'Manzana', 0.00, 0.00, 0, 'C', 'D', 'E', 1),
-(277, '56', 'Uvas', 0.00, 0.00, 0, 'C', '', 'E', 1),
-(278, '75', 'Limón', 0.00, 0.00, 0, '', 'A', '', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -424,14 +297,6 @@ CREATE TABLE `proveedores` (
   `telefono` varchar(15) NOT NULL,
   `estado` int(10) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `proveedores`
---
-
-INSERT INTO `proveedores` (`id`, `nombre`, `direccion`, `telefono`, `estado`) VALUES
-(1, 'Genérico', 'NA', 'NA', 1),
-(6, 'Prueba2', 'AAAA', '3123123123', 1);
 
 -- --------------------------------------------------------
 
@@ -455,10 +320,10 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `correo`, `rol`, `clave`, `estado`, `perfil`) VALUES
-(14, '111', 'Jordán Rodríguez', 'admin@ucol.mx', '5', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, 'perfil.jpg'),
-(26, '666', 'Prueba1', 'prueba1@ucol.mx', '4', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, 'perfil.jpg'),
-(27, '999', 'Prueba2', 'prueba2@ucol.mx', '3', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, 'perfil.jpg'),
-(28, '333', 'Prueba3', 'prueba3@ucol.mx', '2', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', 1, 'perfil.jpg');
+(1, '20230511', 'Sheldon Lee Cooper', 'admin@ucol.mx', '5', '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(2, '20230512', 'Elon Reeve Musk', 'gestor@ucol.mx', '4', '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(3, '20230513', 'Alan Mathison Turing', 'vend@ucol.mx', '3', '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg'),
+(4, '20230514', 'William Henry Gates III', 'resp@ucol.mx', '2', '4f102cc702a91a8fa8ec2b641371afc92a556f7e690444d7498215b5bd0b6342', 1, 'perfil.jpg');
 
 -- --------------------------------------------------------
 
@@ -476,35 +341,6 @@ CREATE TABLE `ventas` (
   `estado` tinyint(4) NOT NULL DEFAULT 1,
   `formato` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
-
---
--- Volcado de datos para la tabla `ventas`
---
-
-INSERT INTO `ventas` (`id`, `descripcion`, `total`, `id_responsable`, `id_generador`, `fecha`, `estado`, `formato`) VALUES
-(44, 'Holaaaa', 200.00, '26', '14', '2023-02-22 23:58:59', 1, ''),
-(45, 'Holaaaa', 200.00, '26', '14', '2023-02-22 23:59:11', 1, ''),
-(46, 'S/D', 300.00, '26', '14', '2023-02-23 00:02:26', 1, ''),
-(47, 'Prueba', 100.00, '26', '14', '2023-03-24 10:11:57', 1, ''),
-(48, 'AAAA', 100.00, '14', '26', '2023-04-06 12:41:14', 0, ''),
-(49, 'BBBBB', 100.00, '26', '14', '2023-04-06 12:46:29', 1, ''),
-(50, '1', 100.00, '14', '26', '2023-04-06 13:13:13', 1, ''),
-(51, 'xxxxxxxxx', 100.00, '26', '14', '2023-04-06 13:17:26', 1, ''),
-(52, '5.2 Ejercicio ilwis ', 100.00, '14', '26', '2023-04-06 13:18:17', 1, ''),
-(53, 'DSFDSF', 100.00, '26', '14', '2023-04-06 13:19:39', 1, ''),
-(54, 'ZZZZ', 100.00, '26', '14', '2023-04-06 13:21:50', 1, ''),
-(55, 'ZZZZ', 100.00, '26', '14', '2023-04-06 13:22:27', 1, ''),
-(56, '222', 20.00, '14', '26', '2023-04-06 13:23:23', 2, ''),
-(58, 'Prueba Ventas', 220.00, '26', '14', '2023-04-13 13:08:04', 1, ''),
-(59, 'Esto es una prueba de salida', 100.00, '26', '14', '2023-04-14 13:18:46', 1, ''),
-(60, 'Hola Esta Es Una Prueba', 220.00, '26', '14', '2023-04-14 13:29:55', 1, ''),
-(61, 'Otra Prueba Fea', 420.00, '26', '14', '2023-04-14 13:32:32', 1, ''),
-(62, 'Funciona la madre', 100.00, '26', '14', '2023-04-14 13:38:10', 1, ''),
-(63, 'Hola', 100.00, '26', '14', '2023-04-14 20:29:30', 1, ''),
-(64, 'Ejemplo de rechazo', 100.00, '14', '26', '2023-04-15 00:37:53', 1, ''),
-(65, 'Ejemplo de aprobado', 220.00, '14', '26', '2023-04-15 00:38:12', 1, ''),
-(67, 'Holaaaaaa', 600.00, '26', '14', '2023-04-16 15:04:20', 1, ''),
-(68, 'Jotos Todos', 200.00, '26', '14', '2023-04-26 10:31:25', 2, '');
 
 --
 -- Índices para tablas volcadas
@@ -620,25 +456,25 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `alumnos`
 --
 ALTER TABLE `alumnos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `asistencias`
 --
 ALTER TABLE `asistencias`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `categorias`
 --
 ALTER TABLE `categorias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `compras`
 --
 ALTER TABLE `compras`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `configuracion`
@@ -650,67 +486,67 @@ ALTER TABLE `configuracion`
 -- AUTO_INCREMENT de la tabla `cotizaciones`
 --
 ALTER TABLE `cotizaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_compra`
 --
 ALTER TABLE `detalle_compra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_coti`
 --
 ALTER TABLE `detalle_coti`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_temp`
 --
 ALTER TABLE `detalle_temp`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=334;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `plantillas`
 --
 ALTER TABLE `plantillas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `practicas`
 --
 ALTER TABLE `practicas`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=279;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedores`
 --
 ALTER TABLE `proveedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
